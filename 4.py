17ed907ff0dcb9a30591434192354d29d4795b4e
Vailow2077 <vailow2077@icloud.com>
2025-08-04 16:38:46 +0300

    added history
    now you can view your history

--- 4.py
+++ 4.py
@@ -1,11 +1,33 @@
 import time
-
-# Получение текущего времени в формате timestamp
-timestamp = time.time()
-
-print("Текущий timestamp:", timestamp)
-
+##
+### Получение текущего времени в формате timestamp
+##timestamp = time.time()
+##
+##print("Текущий timestamp:", timestamp)
+##
 from datetime import datetime
+##
+##real_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+##print("Текущее время:", real_time)
 
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print("Текущее время:", current_time)
+
+
+# Ввод и преобразование дат
+
+while True:
+    start_date_str = input("Введите начальную дату (в формате ГГГГ-ММ-ДД): ")
+    try:
+        start_ts = int(datetime.strptime(start_date_str, "%Y-%m-%d").timestamp())
+        break
+    except ValueError:
+        print("Неправильный формат даты. Используй ГГГГ-ММ-ДД.")
+
+while True:
+    end_date = input()
+    try:
+        end_ts = int(datetime.strptime(end_date, "%Y-%m-%d").replace(hour=23, minute=59, second=59).timestamp())
+        break
+    except ValueError:
+        print()
+
+
